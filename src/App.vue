575b7c6b216a365adf8c3a01fc60745d7941b3ba
Explicit12 <explicit1723@gmail.com>
2023-04-26 11:41:27 +0300

    added button component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+  import AppButton from "./components/AppButton.vue";
+</script>
+
 <template>
-  <main>
-    <h1 class="text-semi-bold text-lg text-primary">Hello world</h1>
+  <main class="container">
+    <h1 class="container__headline text-semi-bold text-lg text-primary">Click button below to open pop-up</h1>
+    <AppButton>Invite User</AppButton>
   </main>
 </template>
+
+<style scoped>
+  .container {
+    margin: 0 auto;
+    max-width: var(--br-xl);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  defineProps<{ icon?: boolean }>();
+</script>
+
+<template>
+  <button
+    class="button text text-md text-white text-semi-bold"
+    :class="{ button_icon: icon, 'button_primary-bg': !icon }"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    outline: none;
+    border: none;
+    background: none;
+
+    padding: var(--gap-3) var(--gap-5);
+    border-radius: var(--radius-lg);
+  }
+
+  .button_primary-bg {
+    background-color: var(--c-primary);
+  }
+
+  .button_icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .button:hover {
+    cursor: pointer;
+  }
+
+  .button:focus {
+    outline: 5px solid var(--c-border);
+  }
+</style>
