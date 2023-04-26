599afb9564157881f18c7e50cde61c700fe755ab
Explicit12 <explicit1723@gmail.com>
2023-04-26 12:55:40 +0300

    added modal component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+  import { ref } from "vue";
+
   import AppButton from "./components/AppButton.vue";
+  import AppModal from "./components/AppModal.vue";
+
+  const isModelOpen = ref(false);
 </script>
 
 <template>
   <main class="container">
     <h1 class="container__headline text-semi-bold text-lg text-primary">Click button below to open pop-up</h1>
-    <AppButton>Invite User</AppButton>
+    <AppButton @click="() => (isModelOpen = true)">Invite User</AppButton>
+
+    <Teleport to="body">
+      <AppModal :is-open="isModelOpen" />
+    </Teleport>
   </main>
 </template>
 
---
+++ src/components/AppModal.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  interface Props {
+    isOpen?: boolean;
+  }
+
+  withDefaults(defineProps<Props>(), { isOpen: false });
+</script>
+
+<template>
+  <Transition name="background">
+    <div class="modal" v-if="isOpen">
+      <div class="modal__content">
+        <slot />
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+  .modal {
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
+
+    max-height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    padding-top: var(--gap-9);
+    box-sizing: border-box;
+
+    background-color: rgba(18, 18, 18, 0.1);
+    backdrop-filter: blur(8px);
+  }
+
+  .modal__content {
+    padding: var(--gap-7);
+    height: fit-content;
+
+    background-color: var(--white);
+    border-radius: var(--radius-md);
+    box-shadow: var(--shadow-md);
+  }
+
+  .background-enter-active,
+  .background-leave-active {
+    transition: background-color 250ms ease, opacity 500ms ease;
+  }
+
+  .background-enter-from,
+  .background-leave-to {
+    background-color: rgba(18, 18, 18, 0);
+    opacity: 0;
+  }
+</style>
