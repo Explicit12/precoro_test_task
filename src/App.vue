dd05a4a21d16c56f4d7964b556ec792b272d361c
Explicit12 <explicit1723@gmail.com>
2023-04-27 19:47:07 +0300

    moved modal window to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-  import { ref } from "vue";
+  import { ref, defineAsyncComponent } from "vue";
 
   import AppButton from "./components/AppButton.vue";
-  import AppModal from "./components/AppModal.vue";
-  import IconClose from "./components/icons/IconClose.vue";
-  import TabButtonGroup from "./components/TabButtonGroup.vue";
+  
+  const InviteUserModal = defineAsyncComponent(() => import("./components/modals/InviteUserModal.vue"));
 
   const isModelOpen = ref(false);
-
-  const tabs = new Set(["Main Info", "Available Locations", "Roles"]);
-  const currentTab = ref([...tabs][0]);
 </script>
 
 <template>
@@ -18,35 +14,7 @@
     <AppButton @click="() => (isModelOpen = true)">Invite User</AppButton>
 
     <Teleport to="body">
-      <AppModal class="modal" :is-open="isModelOpen">
-        <template #header>
-          <header class="modal__header">
-            <h2 class="text-semi-bold text-lg text-primary">Invite User</h2>
-            <AppButton icon aria-label="Close button">
-              <IconClose class="close-icon" @click="() => (isModelOpen = false)" />
-            </AppButton>
-          </header>
-          <TabButtonGroup :tabs="tabs" v-model="currentTab" />
-        </template>
-
-        <template #content>
-          <div class="content-modal">
-            <p class="text-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, impedit! Rerum maxime aut recusandae iure
-              repellat accusantium, voluptatem quae delectus, sequi rem incidunt voluptates fugiat molestiae asperiores
-              suscipit. Excepturi, eligendi.
-            </p>
-          </div>
-        </template>
-
-        <template #footer>
-          <p class="text-medium">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, impedit! Rerum maxime aut recusandae iure
-            repellat accusantium, voluptatem quae delectus, sequi rem incidunt voluptates fugiat molestiae asperiores
-            suscipit. Excepturi, eligendi.
-          </p>
-        </template>
-      </AppModal>
+      <InviteUserModal :is-open="isModelOpen" @close-click="() => (isModelOpen = false)" />
     </Teleport>
   </main>
 </template>
@@ -60,22 +28,5 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
-
-  .close-icon {
-    fill: var(--c-secondary-dark);
-    stroke: var(--c-secondary-dark);
-    width: 2rem;
-    height: auto;
-  }
-
-  .modal__header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .content-modal {
-    display: flex;
   }
 </style>
